6eca0351a9e60d7126a38eb9df15201eff855fb7
zheyaoa <1578127412@qq.com>
2018-11-13 21:10:31 +0800

    add some js demo

---
+++ Array/unique.js
@@ -0,0 +1,13 @@
+let unique = (str) => {
+    let chars = new Array();
+    let strs = [...str];
+    for(var i = 0;i<strs.length; i++){
+        if(chars[strs[i]-'a'] == undefined){ 
+            chars[strs[i]-'a'] = 1;
+        }else {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(unique('hello'))
